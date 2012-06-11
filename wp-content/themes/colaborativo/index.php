706b1463d2aef8776b5a8aca41f9489f8e14d381
Anabelle Handdoek <ana@gathacol.net>
2012-06-10 22:45:45 -0500

    hero home

--- wp-content/themes/colaborativo/index.php
+++ wp-content/themes/colaborativo/index.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
-YIKES
+<div class="hero-unit">
+	<div class="row">
+		<div class="span6">
+			<h1>Hello, world!</h1>
+			<h2>This is a template for event covering and real-time social network agregation with a cool style.</h2>
+		</div>
+		<div class="span4 aright">
+			Event icon
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
