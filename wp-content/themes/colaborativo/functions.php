1b061f05835cb1a95edbe2c3c063f6021ebc61e1
Anabelle Handdoek <ana@gathacol.net>
2012-06-19 23:52:47 -0500

    paging basic

--- wp-content/themes/colaborativo/functions.php
+++ wp-content/themes/colaborativo/functions.php
@@ -351,6 +351,7 @@ function colaborativo_content_nav( ) {
 
     if ( $wp_query->max_num_pages > 1 ) : ?>
         <nav id="<?php echo $nav_id; ?>">
+            <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyeleven' ) ); ?></div>
             <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></div>
         </nav><!-- #nav-above -->
     <?php endif;
