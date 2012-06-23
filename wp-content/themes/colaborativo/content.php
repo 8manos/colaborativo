3dc870a3cbb63bcc5f25a32bb559e21e12619068
Anabelle Handdoek <ana@gathacol.net>
2012-06-23 03:11:03 -0500

    separate single content

---
+++ wp-content/themes/colaborativo/content.php
@@ -0,0 +1,1 @@
+<?php display_article(); ?>
---
+++ wp-content/themes/colaborativo/single.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<?php
+
+	while (have_posts()) : the_post();
+
+		$post_type = get_post_type();
+
+		get_template_part('content', $post_type);
+
+	endwhile;
+?>
+<?php get_footer(); ?>
