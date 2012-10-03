bf27f1a0c15a1396c2d682f4ce17f9b5517d6a12
Anabelle Handdoek <ana@gathacol.net>
2012-10-03 12:14:50 -0500

    disable single link

--- wp-content/themes/colaborativo/single.php
+++ wp-content/themes/colaborativo/single.php
@@ -1,19 +1,4 @@
 <?php 
-	$is_ajax = $_GET['ajax'];
-	
-	if( !$is_ajax ){
-		get_header(); 
-	}
-
-	while (have_posts()) : the_post();
-
-		$post_type = get_post_type();
-
-		get_template_part('content', $post_type);
-
-	endwhile;
-
-	if( !$is_ajax ){
-		get_footer(); 
-	}
+	$redirect_url = get_bloginfo('url')."#post-".get_the_ID();
+	wp_redirect( $redirect_url , $status = 302)
 ?>
