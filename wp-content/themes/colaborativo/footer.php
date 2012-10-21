9c59504ea8c95b375e6a4e26ab9946bbac6f0612
Anabelle Handdoek <github@huellaspyp.com>
2012-10-20 19:20:43 -0500

    multisubdomain analytics

--- wp-content/themes/colaborativo/footer.php
+++ wp-content/themes/colaborativo/footer.php
@@ -41,12 +41,19 @@
         <button class="btn" data-dismiss="modal" aria-hidden="true">Cerrar</button>
       </div>
     </div>
+    <script type="text/javascript">
 
-    <script>
-        var _gaq=[['_setAccount','UA-32985059-1'],['_trackPageview']];
-        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g,s)}(document,'script'));
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-32985059-1']);
+      _gaq.push(['_setDomainName', 'colaborativo.co']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
     </script>
     <?php wp_footer(); ?>
     <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
