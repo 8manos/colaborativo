6ae94b439e2598798c5be7aa112dba0e9142e298
Anabelle Handdoek <github@huellaspyp.com>
2013-06-25 14:34:21 -0600

    filtros desplegables en plantilla1

--- wp-content/themes/colaborativo/index.php
+++ wp-content/themes/colaborativo/index.php
@@ -51,33 +51,7 @@
 </div>
 
 <div class="subnav row" id="filters">
-	<div class="span3">
-		<h3>Filtre el contenido por:</h3>
-	</div>
-
-	<div class="span6">
-		<div class="btn-group" id="filters-buttons">
-		<?php
-		$args=array(
-		  'public'   => true,
-		  '_builtin' => false
-		);
-		$output = 'names'; // names or objects, note names is the default
-		$operator = 'and'; // 'and' or 'or'
-		$post_types=get_post_types($args,$output,$operator);
-		array_push($post_types, 'post');
-		array_reverse($post_types);
-			  foreach ($post_types  as $post_type ) {
-			  	$link = add_query_arg( 'post_type', $post_type );
-			    echo '<a class="ir sprite tipo-'. $post_type .'" href="'.$link.'" class="btn'. $post_type .'">'. $post_type. '</a>';
-			  }
-		?>
-		</div>
-	</div>
-
-	<div class="span3 aright">
-
-	</div>
+	<?php get_template_part( 'filters', $plantilla ); ?>
 </div>
 
 <div id="counter_wrapper">
