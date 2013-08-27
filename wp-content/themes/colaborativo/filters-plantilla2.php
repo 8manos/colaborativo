bf2ac544be92b4b9dd9bd2a191f00aae7c24a472
Anabelle Handdoek <github@huellaspyp.com>
2013-08-26 23:16:18 -0500

    filters plantilla 2

---
+++ wp-content/themes/colaborativo/filters-plantilla2.php
@@ -0,0 +1,23 @@
+	<div class="span3">
+		<h3>Filtre el contenido por:</h3>
+	</div>
+
+	<div class="span9">
+		<div class="btn-group" id="filters-buttons">
+		<?php
+		$args=array(
+		  'public'   => true,
+		  '_builtin' => false
+		);
+		$output = 'names'; // names or objects, note names is the default
+		$operator = 'and'; // 'and' or 'or'
+		$post_types=get_post_types($args,$output,$operator);
+		array_push($post_types, 'post');
+		array_reverse($post_types);
+			  foreach ($post_types  as $post_type ) {
+			  	$link = add_query_arg( 'post_type', $post_type );
+			    echo '<a class="ir sprite tipo-'. $post_type .'" href="'.$link.'" class="btn'. $post_type .'">'. $post_type. '</a>';
+			  }
+		?>
+		</div>
+	</div>
