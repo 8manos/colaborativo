c55afdfac5117a96d45b2ba38d20696d4c8a074d
Anabelle Handdoek <github@huellaspyp.com>
2013-08-26 23:22:52 -0500

    need some sleep

--- wp-content/themes/colaborativo/index.php
+++ wp-content/themes/colaborativo/index.php
@@ -48,7 +48,7 @@
 
 <div class="row colaborated">
 	<h3 class="span12">
-		<?php if( $plantilla = 'plantilla3' ){ ?>
+		<?php if( $plantilla == 'plantilla3' ){ ?>
 			Actividades
 		<?php }else{ ?>
 			Contenido generado por los asistentes
